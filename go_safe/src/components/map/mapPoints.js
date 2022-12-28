91be22bf38705155b67079455966b79705bba175
ntanjila <nahidaakter@iut-dhaka.edu>
2022-12-28 21:48:58 +0600

    avoiding severe points in map

--- go_safe/src/components/map/mapPoints.js
+++ go_safe/src/components/map/mapPoints.js
@@ -28,20 +28,9 @@ var myIcon = L.icon({
   iconUrl: icon,
 });
 
-var greenIcon = new L.Icon({
-  iconUrl:
-    "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
-
 var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
-  attribution: "© OpenStreetMap",
+  // attribution: "© OpenStreetMap",
 });
 
 class Map extends React.Component {
@@ -72,7 +61,9 @@ class Map extends React.Component {
     var routeControl = L.Routing.control({
       show: true,
       fitSelectedRoutes: true,
+      showAlternatives: true,
       plan: false,
+     // waypoints: waypoints.spliceWaypoints(1,11),
       lineOptions: {
         styles: [
           {
@@ -90,17 +81,103 @@ class Map extends React.Component {
 
     // console.log(lang, lat)
 
-    var newLatLngA = new L.LatLng(lang, lat, "taskA");
+    var newLatLngA = new L.LatLng(lat,lang, "taskA");
     console.log(newLatLngA);
-    var newLatLngB = new L.LatLng(lang1, lat1, "taskB");
+    var newLatLngB = new L.LatLng( lat1, lang1, "taskB");
     console.log(newLatLngB);
     // var newLatLngC = new L.LatLng(23.9469787, 90.3774195, "taskc");
     // var newLatLngD = new L.LatLng(23.905446661730625, 90.39890486065245
     //     , "taskc");
+   
+ //routeControl.setWaypoints([newLatLngA, newLatLngB]);
+    var latArr = [];
+    var i = 1;
+    latArr.push(newLatLngA);
+    const pointsGeoJson = new L.GeoJSON(pointsJson, {
+      onEachFeature: (feature = {}, layer) => {
+        const { properties = {} } = feature;
+        const { geometry = {} } = feature;
 
-    //routeControl.setWaypoints([newLatLngA, newLatLngB, newLatLngC, newLatLngD]);
+        const { level } = properties;
+        const { coordinates } = geometry;
+        // console.log(geometry);
+        // console.log(coordinates[0]);
+        
 
-    routeControl.setWaypoints([newLatLngA, newLatLngB]);
+        latArr.push(new L.LatLng(coordinates[1], coordinates[0], "taskU"));
+
+        
+        if (!level) return;
+
+        layer.bindPopup(`Severity level:<b> ${level}</b>`);
+      },
+    });
+    var len = latArr.length;
+    latArr.push(newLatLngB);
+    console.log(latArr);
+    for (i = 0; i < latArr.length-1; i++) {
+     
+      //console.log(latArr[i+1]);
+    }
+    //routeControl.setWaypoints(latArr);
+    L.Routing.control({
+      waypoints : latArr, 
+      showAlternatives: true,
+      allowUTurn: true
+    }).addTo(this.map).spliceWaypoints(1,12);
+    // L.Routing.control({
+    //   waypoints : [newLatLngA, newLatLngB], 
+    //   showAlternatives: true,
+    // }).addTo(this.map);
+//uttara high school 23.87137429464117, 90.39682068149936
+    //routeControl.setWaypoints([newLatLngA, 
+      //  L.latLng(
+      // 23.873813453585864, 90.39714507019949),
+     // L.latLng(latArr[0].lat, latArr[0].lng),
+            //   L.latLng(latArr[1].lat, latArr[1].lng),
+            //   L.latLng(latArr[2].lat, latArr[2].lng),
+            //   L.latLng(latArr[3].lat, latArr[3].lng),
+            //   L.latLng(latArr[4].lat, latArr[4].lng),
+            //   L.latLng(latArr[5].lat, latArr[5].lng),
+            //   L.latLng(latArr[6].lat, latArr[6].lng),
+            //   L.latLng(latArr[7].lat, latArr[7].lng),
+            //   L.latLng(latArr[8].lat, latArr[8].lng),
+            //   L.latLng(latArr[9].lat, latArr[9].lng),
+            //   L.latLng(latArr[10].lat, latArr[10].lng),
+            //  L.latLng(latArr[11].lat, latArr[11].lng)
+           //  ,newLatLngB]);
+    // L.Routing.control({
+    //       waypoints: [
+    //         newLatLngA,
+    //         L.latLng(
+    //                23.873813453585864, 90.39714507019949),
+    //         //L.latLng(latArr[0].lat, latArr[0].lng),
+    //         // L.latLng(latArr[1].lat, latArr[1].lng),
+    //         // L.latLng(latArr[2].lat, latArr[2].lng),
+    //         // L.latLng(latArr[3].lat, latArr[3].lng),
+    //         // L.latLng(latArr[4].lat, latArr[4].lng),
+    //         // L.latLng(latArr[5].lat, latArr[5].lng),
+    //         // L.latLng(latArr[6].lat, latArr[6].lng),
+    //         // L.latLng(latArr[7].lat, latArr[7].lng),
+    //         // L.latLng(latArr[8].lat, latArr[8].lng),
+    //         // L.latLng(latArr[9].lat, latArr[9].lng),
+    //         // L.latLng(latArr[10].lat, latArr[10].lng),
+    //        // L.latLng(latArr[11].lat, latArr[11].lng),
+    //         newLatLngB
+    //       ],
+    //       showAlternatives: true,
+    //     }).addTo(this.map).spliceWaypoints(0, 2);
+
+    console.log(latArr[3].lat, latArr[3].lng);
+    // L.Routing.control({
+    //   waypoints: [
+    //     L.latLng(latArr[3].lat, latArr[3].lng,),
+    //     L.latLng(latArr[4].lat, latArr[4].lng,)
+    //   ]
+    // }).addTo(this.map);
+    
+    pointsGeoJson.addTo(this.map);
+
   }
   componentDidMount() {
     if (this.map) {
@@ -135,89 +212,26 @@ class Map extends React.Component {
     );
     layerControl.addBaseLayer(Stadia_AlidadeSmoothDark, "dark");
 
-    // const pointsGeoJson = new L.GeoJSON(pointsJson, {
-    //   onEachFeature: (feature = {}, layer) => {
-    //     const { properties = {} } = feature;
-    //     const { level } = properties;
-
-    //     if (!level) return;
-
-    //     layer.bindPopup(`Severity level:<b> ${level}</b>`);
-    //     console.log("showing");
-    //   },
-    //   pointToLayer: (feature = {}, latlng) => {
-    //     console.log("marker showing");
-    //     return L.marker(latlng, { icon: myIcon });
-    //   }
-    // });
-    // pointsGeoJson.addTo(this.map);
-    var routeControl = L.Routing.control({
-      show: true,
-      fitSelectedRoutes: true,
-      plan: false,
-      lineOptions: {
-        styles: [
-          {
-            color: "blue",
-            opacity: "0.7",
-            weight: 6,
-          },
-        ],
-      },
-    })
-      .addTo(this.map)
-      .getPlan();
-    var latArr = [];
-    var i = 1;
-    const pointsGeoJson = new L.GeoJSON(pointsJson, {
-      onEachFeature: (feature = {}, layer) => {
-        const { properties = {} } = feature;
-        const { geometry = {} } = feature;
-
-        const { level } = properties;
-        const { coordinates } = geometry;
-        console.log(geometry);
-        console.log(coordinates[0]);
-
-        latArr.push(new L.LatLng(coordinates[0], coordinates[1], "taskU"));
-        // console.log(newLatLngA);
-        // var newLatLngB = new L.LatLng(39.952812, -83.119293, "taskA");
-
-        // routeControl.setWaypoints([newLatLngA, newLatLngB]);
-
-        if (!level) return;
-
-        layer.bindPopup('Severity level:<b> ${level}</b>');
-      },
-    });
-    console.log(latArr);
-    for (i = 0; i < latArr.length; i++) {
-      routeControl.setWaypoints([latArr[i], latArr[i + 1], latArr[i + 2]]);
-      console.log(latArr[i]);
-    }
-
-    pointsGeoJson.addTo(this.map);
+    
+      
+      // L.Routing.control({
+      //   waypoints: [
+      //     L.latLng(
+      //       23.87459218969208, 90.39680536110893),
+      //     // L.latLng(
+      //     //   23.87682132593331, 90.3972130668656),
+  
+      //     L.latLng(
+      //       23.87633482164616, 90.39936057486561)
+      //   ]
+      //   ,
+      //    showAlternatives: true
+      //   //show: false
+      // }).addTo(this.map);
+  
 
     L.Marker.prototype.options.icon = myIcon;
   }
-
-  addMarker = (e) => {
-    const markers_ = this.state.markers;
-    markers_.push(e);
-    this.setState({ markers_ });
-  };
-
-  selectPoints = (lat, lng) => {
-    const radius = this.radius;
-    const current_position = L.latLng(lat, lng);
-
-    this.state.markers.forEach((m) => {
-      const userLocation = [m.latlng.lat, m.latlng.lng];
-      const distance = current_position.distanceTo(userLocation);
-
-      m.inRadius = distance <= radius;
-    });
-  };
 
   Form = () => {
     const [inputs, setInputs] = useState({});
@@ -241,42 +255,42 @@ class Map extends React.Component {
     return (
       <form onSubmit={handleSubmit}>
         <label>
-          Enter source langitude
+          Enter source latitude 
           <input
-            type="number"
-            name="langitude"
-            value={inputs.langitude || ""}
-            onChange={handleChange}
-            id="langitude"
-          />
-        </label>
-        <label>
-          Enter source latitude
-          <input
-            id="latitude"
             type="number"
             name="latitude"
             value={inputs.latitude || ""}
             onChange={handleChange}
+            id="latitude"
           />
         </label>
         <label>
-          Enter destination langitude
+          Enter source langitude
           <input
+            id="langitude"
             type="number"
-            name="langitude1"
-            value={inputs.langitude1 || ""}
+            name="langitude"
+            value={inputs.langitude || ""}
             onChange={handleChange}
-            id="langitude1"
           />
         </label>
         <label>
           Enter destination latitude
           <input
-            id="latitude1"
             type="number"
             name="latitude1"
             value={inputs.latitude1 || ""}
+            onChange={handleChange}
+            id="latitude1"
+          />
+        </label>
+        <label>
+          Enter destination langitude 
+          <input
+            id="langitude1"
+            type="number"
+            name="langitude1"
+            value={inputs.langitude1 || ""}
             onChange={handleChange}
           />
         </label>
@@ -294,9 +308,7 @@ class Map extends React.Component {
           <this.Form />
         </div>
 
-        <div id="map" style={style}>
-          {/* <div className="pointer" /> */}
-        </div>
+        <div id="map" style={style}></div>
       </div>
     );
   }
